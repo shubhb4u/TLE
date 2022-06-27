c1db2c686ca6f605a52b418bd8765034f74a246f
shubhb4u <87018916+shubhb4u@users.noreply.github.com>
2022-06-27 16:39:51 -0500

    Add files via upload

--- Tom_Riddle.cpp
+++ Tom_Riddle.cpp
@@ -31,3 +31,41 @@ int main() {
         else cout << "NO" << endl;
     }
 }
+
+
+// Approach 2 - first count if the name is present and then add the names to a set in the end
+
+
+//#include<bits/stdc++.h>
+#include<iostream>
+#include<cmath>
+#include <algorithm>
+#include<numeric>
+#include<vector>
+#include<map>
+#include <array>
+#include<iomanip>
+#include <set>
+#include<limits.h>
+const int MOD = 1e9 + 7;
+using namespace std;
+typedef long long ll;
+
+
+int main() {
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);cout.tie(NULL);
+
+    int n;
+    cin >> n;
+    set<string> s;
+    while(n--){
+        string name;
+        cin >> name;
+
+        (s.find(name) != s.end()) ? cout << "YES"<< endl : cout << "NO" << endl;
+        s.insert(name);
+    }
+
+}
